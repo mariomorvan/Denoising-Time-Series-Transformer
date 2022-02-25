12eb4de97ae41a3f66049976a685f3565b686525
mariomorvan <mario.morvan.18@ucl.ac.uk>
2022-02-25 11:40:28 +0000

    add validation path

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ def add_arguments(parser):
 
     # loading/training/eval stages
     parser.add_argument('--train_path')
-    parser.add_argument('--test_path')
-    parser.add_argument("--checkpoint")  # not activated yet
-    parser.add_argument("--eval", action="store_true", help="Whether to eval "
-                        + "on the full test dataset or not")
+    parser.add_argument(
+        '--val_path', help='path to the directory containing the validation samples.')
+    parser.add_argument('--test_path', help='path to the directory containing the testing samples.'
+                        ' Will activate evaluation on test set.')
+    parser.add_argument('--checkpoint')
 
     # project, checkpoints
     parser.add_argument("--name", type=str, default=None,
@@ -42,7 +43,6 @@ def add_arguments(parser):
 
     # data options
     parser.add_argument('--max_samples', type=int, default=None)
-
     parser.add_argument('--val_ratio', type=float, default=0.1,
                         help="fraction of samples used for validation.")
     parser.add_argument('--seed', type=int, default=0)
@@ -50,11 +50,14 @@ def add_arguments(parser):
     # Transforms
     parser.add_argument('--crop', default=500, type=int)
     parser.add_argument('--downsample', default=0, type=int)
+    parser.add_argument('--skip', default=25, type=int, help="Defines size of the tails skipped"
+                        + " when stitching multiple predictions")
 
     # general training options
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--num_workers", type=int, default=0)
-    parser.add_argument('--epochs', type=int, default=10000)
+    parser.add_argument('--epochs', "--max_epochs", type=int, default=10000)
+    parser.add_argument('--min_epochs', type=int, default=0)
     parser.add_argument('--monitor', help='metrics used '
                         + 'for checkpointing and early stopping')
     parser.add_argument('--patience', type=float)
@@ -69,9 +72,6 @@ def add_arguments(parser):
     parser.add_argument("--block_len", type=int, default=5,
                         help='Average masking block lengths.')
     return parser
-
-
-skip = 25
 
 
 if __name__ == '__main__':
@@ -108,62 +108,41 @@ if __name__ == '__main__':
                                 value=None, exclude_mask=True, max_ratio=0.95)]
     transform_list += [StandardScaler(dim=0)]
     transform_list_2 += [StandardScaler(dim=0)]
-
     transform_both_train = Compose(transform_list)
-
     transform_both_2 = Compose(transform_list_2)
-
     transform = None
 
-    dataset = TessDataset(args.train_path,
-                          load_processed=True,
-                          max_samples=args.max_samples,
-                          transform=transform,
-                          transform_both=transform_both_train,
-                          use_cache=True,
-                          )
-    test_dataset = TessDataset(args.test_path,
-                               load_processed=True,
-                               use_cache=True,
-                               )
+    train_dataset = TessDataset(args.train_path,
+                                load_processed=True,
+                                max_samples=args.max_samples,
+                                transform=transform,
+                                transform_both=transform_both_train,
+                                use_cache=True,
+                                shuffle=True
+                                )
 
-    #dataset.n_dim = 1
-    # TRAIN/VAL SPLIT
-    val_size = int(args.val_ratio * len(dataset))
-    train_size = len(dataset) - val_size
-    train_indices, val_indices = split_indices((train_size, val_size),
-                                               generator=torch.Generator().manual_seed(args.seed))
-    train_dataset = Subset(dataset, train_indices)
+    if args.val_path:
+        val_dataset1 = TessDataset(args.val_path,
+                                   load_processed=True,
+                                   transform=transform,
+                                   transform_both=transform_both_train,
+                                   use_cache=True,
+                                   )
+        val_dataset2 = Subset(
+            val_dataset1, replace_transform_both=transform_both_2)
 
-    val_dataset1 = Subset(dataset, val_indices)
-    val_dataset2 = Subset(dataset, val_indices,
-                          replace_transform_both=transform_both_2)
-
-    test_dataset1 = Subset(
-        test_dataset, replace_transform_both=transform_both_train)
-    test_dataset2 = Subset(
-        test_dataset, replace_transform_both=transform_both_2)
+        val_loader1 = DataLoader(val_dataset1, batch_size=batch_size, shuffle=False,
+                                 num_workers=num_workers, pin_memory=pin_memory)
+        val_loader2 = DataLoader(val_dataset2, batch_size=batch_size, shuffle=False,
+                                 num_workers=num_workers, pin_memory=pin_memory)
+    elif args.val_ratio:
+        raise NotImplementedError
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
                               num_workers=num_workers, pin_memory=pin_memory)
-    val_loader1 = DataLoader(val_dataset1, batch_size=batch_size, shuffle=False,
-                             num_workers=num_workers, pin_memory=pin_memory)
-    val_loader2 = DataLoader(val_dataset2, batch_size=batch_size, shuffle=False,
-                             num_workers=num_workers, pin_memory=pin_memory)
-    test_loader1 = DataLoader(test_dataset1, batch_size=batch_size, shuffle=False,
-                              num_workers=num_workers, pin_memory=pin_memory)
-    test_loader2 = DataLoader(test_dataset2, batch_size=batch_size, shuffle=False,
-                              num_workers=num_workers, pin_memory=pin_memory)
 
-    loader_pred = DataLoader(test_dataset,
-                             batch_size=1,
-                             shuffle=False,
-                             collate_fn=CollatePred(
-                                 args.crop, step=args.crop - skip * 2),
-                             num_workers=num_workers, pin_memory=pin_memory)
-
-    print('train size:', len(train_dataset), '\ntest size:',
-          len(test_dataset1), '\nval size:', len(val_dataset1))
+    print('train size:', len(train_dataset))
+    print('val size:', len(val_dataset1))
 
     # MODEL DEF AND TRAIN`
     dict_args = vars(args).copy()
@@ -171,13 +150,18 @@ if __name__ == '__main__':
         if dict_args[k] is None:
             dict_args.pop(k)
     lit_model = LitImputer(**dict_args)
+    if args.checkpoint:
+        lit_model = lit_model.load_from_checkpoint(args.checkpoint)
+        print('model loaded from', args.checkpoint)
+
     if args.neptune_project:
         from pytorch_lightning.loggers import NeptuneLogger
         logger = NeptuneLogger(project=args.neptune_project,
                                name='tess_denoising' if args.name is None else args.name,
                                log_model_checkpoints=False,
-                               tags=[str(len(dataset))+' samples']
-                               + (args.tags if args.tags is not None else []))
+                               tags=[str(len(train_dataset))+' samples']
+                               + (args.tags if args.tags is not None else [])
+                               + (["from ckpt"] if args.checkpoint else []))
     else:
         logger = None
 
@@ -194,6 +178,7 @@ if __name__ == '__main__':
                                        mode='min'))
 
     trainer = pl.Trainer(max_epochs=args.epochs,
+                         min_epochs=args.min_epochs,
                          logger=logger,
                          gpus=GPUS,
                          profiler='simple',
@@ -208,13 +193,32 @@ if __name__ == '__main__':
     except FileNotFoundError as e:
         print(e)
 
-    if args.eval:
+    if args.test_path:
+        test_dataset = TessDataset(args.test_path,
+                                   load_processed=True,
+                                   use_cache=True,
+                                   )
+        test_dataset1 = Subset(
+            test_dataset, replace_transform_both=transform_both_train)
+        test_dataset2 = Subset(
+            test_dataset, replace_transform_both=transform_both_2)
+        test_loader1 = DataLoader(test_dataset1, batch_size=batch_size, shuffle=False,
+                                  num_workers=num_workers, pin_memory=pin_memory)
+        test_loader2 = DataLoader(test_dataset2, batch_size=batch_size, shuffle=False,
+                                  num_workers=num_workers, pin_memory=pin_memory)
+        loader_pred = DataLoader(test_dataset,
+                                 batch_size=1,
+                                 shuffle=False,
+                                 collate_fn=CollatePred(
+                                     args.crop, step=args.crop - args.skip * 2),
+                                 num_workers=num_workers, pin_memory=pin_memory)
         torch.cuda.empty_cache()
-
         trainer.test(lit_model, dataloaders=[test_loader1, test_loader2])
 
         iqr, dw = eval_full_inputs(
-            lit_model, loader_pred, test_dataset, skip, 'cuda')
-        logger.experiment['testing/full_test_iqr'] = iqr
-        logger.experiment['testing/full_test_dw2'] = dw
-        print(iqr, dw)
+            lit_model, loader_pred, test_dataset, args.skip, 'cuda')
+        logger.experiment['testing/full_test_iqr'] = iqr.mean()
+        logger.experiment['testing/full_test_dw2'] = np.abs(dw-2).mean()
+        print('test IQR', logger.experiment['testing/full_test_iqr'])
+        print('test mean absolute DW-2 statistics',
+              logger.experiment['testing/full_test_dw2'])
